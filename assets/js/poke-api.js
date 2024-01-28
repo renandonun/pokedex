e1c825762cd10b40c5c38bef4486f9f359259f64
renandonun <renandonun@gmail.com>
2024-01-28 10:33:40 -0300

    alterações

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,11 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 const pokeApi = {};
 
-pokeApi.getPokemons = () => {
-    
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .catch((error) => console.error(error))
+        .finally(() => console.log('Requisição concluída'))
 }
