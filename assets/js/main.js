16d9cd0e3226e63410e71a1eebb123873d9bfa9a
renandonun <renandonun@gmail.com>
2024-01-30 06:52:05 -0300

    criacao do modelo pokemon e do readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                <li class="type">Grama</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.photo}"
                     alt="${pokemon.name}">
             </div>
         </li>
@@ -19,16 +20,8 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-pokeApi.getPokemons().then((pokemons) => {
-
-    const newList = [];
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        newList.push(convertPokemonToLi(pokemon));    
-    }
-
-    pokemonList.innerHTML = newList.join('');
-
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML = newHtml;
 })
     
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Pokédex
+
+Este é um projeto de uma Pokédex simples desenvolvida com HTML, CSS e JavaScript durante a **Formação JavaScript Developer** da DIO - Digital Innovation One. Foi utilizada a API da *PokeApi*.
+
+## Visão Geral
+
+A Pokédex é uma enciclopédia eletrônica portátil de Pokémon, onde os treinadores podem visualizar informações sobre diferentes espécies de Pokémon.
+
+## Funcionalidade
+
+- Exibição de uma lista de Pokémons com suas respectivas informações.
+
+## Como Usar
+
+1. Clone este repositório para o seu ambiente local:
+
+git clone https://github.com/renandonun/pokedex.git
